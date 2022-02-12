4719852309295dd921172e00b9f6f49127797b6f
JHSeo <qnfqnfqnf@gmail.com>
2022-02-13 02:38:53 +0900

    fix ssr page

--- src/pages/ssr/index.tsx
+++ src/pages/ssr/index.tsx
@@ -2,9 +2,10 @@ import type { GetServerSideProps } from 'next';
 import fetchData from '@/lib/fetch-data';
 import Story from '@/components/Story.client';
 import { Item } from '@/types/types';
+import Layout from '@/components/Layout.client';
 
 export const getServerSideProps: GetServerSideProps = async context => {
-  const storyIds = await fetchData('topstories', 500);
+  const storyIds = await fetchData('topstories');
   const data = await Promise.all<Item[]>(
     storyIds.slice(0, 30).map((id: string) => fetchData(`item/${id}`))
   );
@@ -22,15 +23,11 @@ export type SSRPageProps = {
 
 function SSRPage({ data }: SSRPageProps) {
   return (
-    <div className="p-4 max-w-5xl mx-auto">
-      <h1 className="text-4xl font-bold text-cyan-700">SSR</h1>
-      <p className="text-lg mb-10 text-gray-600">
-        Hacker News API: Top Stories(30)
-      </p>
+    <Layout title="SSR" description="Hacker News API: Top Stories(30)">
       {data.map(item => (
         <Story key={item.id} item={item} />
       ))}
-    </div>
+    </Layout>
   );
 }
 
